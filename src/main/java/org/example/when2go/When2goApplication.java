bf485d22404ee60d522188a3232a58ef869f522a
eclipse021 <yahang2008@naver.com>
2026-04-27 14:16:02 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/org/example/when2go/When2goApplication.java
@@ -0,0 +1,13 @@
+package org.example.when2go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class When2goApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(When2goApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/when2go/When2goApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.when2go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class When2goApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
