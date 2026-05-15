038b019b50ea7d65a721680b32e8a71e0f242c41
743hw4n <taehwan011@gmail.com>
2026-05-15 18:25:44 +0900

    feat: Google Routes API 클라이언트 구현

--- src/main/java/org/example/when2go/domain/route/client/GoogleRouteClient.java
+++ src/main/java/org/example/when2go/domain/route/client/GoogleRouteClient.java
@@ -3,7 +3,7 @@ package org.example.when2go.domain.route.client;
 import org.example.when2go.domain.route.dto.RouteSearchRequest;
 import org.example.when2go.domain.route.dto.RouteSearchResult;
 
-public interface OdsayRouteClient {
+public interface GoogleRouteClient {
 
     RouteSearchResult search(RouteSearchRequest request);
 }
