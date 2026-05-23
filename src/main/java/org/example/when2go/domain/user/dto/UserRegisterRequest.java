1a7e8ef7b953a395228e4276f337f4fe55fb4202
eclipse021 <yahang2008@naver.com>
2026-05-24 03:26:05 +0900

    fix: 유저 가입 request(deviceId, fcmToken)에 조건 추가

--- src/main/java/org/example/when2go/domain/user/dto/UserRegisterRequest.java
+++ src/main/java/org/example/when2go/domain/user/dto/UserRegisterRequest.java
@@ -2,10 +2,15 @@ package org.example.when2go.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.when2go.domain.user.entity.Platform;
 
 public record UserRegisterRequest(
-        @NotBlank String deviceId,
+        @NotBlank
+        @Size(min = 36, max = 36)
+        String deviceId,
         @NotNull Platform platform,
-        @NotBlank String fcmToken
+        @NotBlank
+        @Size(max = 512)
+        String fcmToken
 ) {}
